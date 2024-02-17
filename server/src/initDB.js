25fdaa91ecab49e5a5494128412cb1e9dc6532f1
vicLira <victor.liracarlos@gmail.com>
2024-02-17 18:30:41 -0300

    alterada variaveis de ambiente

--- server/src/initDB.js
+++ server/src/initDB.js
@@ -1,25 +1,49 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-        dbName: process.env.MONGO_INITDB_DATABASE,
-        user: process.env.MONGO_INITDB_ROOT_USERNAME,
-        pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  const mongoUri = process.env.MONGODB_URI; // Prioritize MONGODB_URI
+
+  if (mongoUri) {
+    // Use MONGODB_URI directly for production environments
+    mongoose
+      .connect(mongoUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('Mongodb connected....');
-    })
-    .catch(err => console.log(err.message));
+      })
+      .then(() => {
+        console.log('MongoDB connected using MONGODB_URI...');
+      })
+      .catch(err => console.error('Error connecting:', err.message));
+  } else {
+    // Use credentials for development or if MONGODB_URI is not available
+    const dbName = process.env.MONGO_INITDB_DATABASE;
+    const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+    const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
+    const connectionString = `mongodb://${username}:${password}@localhost:27017/${dbName}?authSource=admin`;
+
+    mongoose
+      .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log(
+          'MongoDB connected using environment credentials...',
+          connectionString
+        );
+      })
+      .catch(err => console.error('Error connecting:', err.message));
+  }
+
+  // Connection event listeners and SIGINT handler remain the same
 
   mongoose.connection.on('connected', () => {
     console.log('Mongoose connected to db...');
   });
 
   mongoose.connection.on('error', err => {
-    console.log(err.message);
+    console.error('Error connecting:', err.message);
   });
 
   mongoose.connection.on('disconnected', () => {
