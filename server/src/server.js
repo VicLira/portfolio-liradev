8d4991e6192c4279d8b5c811ab969dd3702c0b3a
vicLira <victor.liracarlos@gmail.com>
2024-02-20 18:59:04 -0300

    alteracao(netlify.toml) - alterado cors da aplicação hospedada netlify

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [ 'http://localhost:3000', 
             'https://liradev-portfolio.netlify.app' 
-          ] // Permitir apenas solicitações do localhost:3000
+          ],
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true,
+  origin: true,
 }));
 
 // Initialize DB
